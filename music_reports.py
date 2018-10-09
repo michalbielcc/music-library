3bd1d41c508006f275bb64f95bb161d1f5041709
KamilBracki <kamilbracki1@gmail.com>
2018-10-09 14:03:10 +0200

    tablekaDash

--- music_reports.py
+++ music_reports.py
@@ -7,16 +7,16 @@ def import_music(music, filename='text_albums_data.txt'):
     for line in txt:
         i = line.split(',')
         music1.append(i)
-    for i in music1
+    for i in music1:
         music.append(i)
     txt.close()
 
 import_music(music, filename='text_albums_data.txt')
 
 
-from tabulate import tabulate
 
 
-print(tabulate(music,["Album", "Artist", "Year", "Genre", "Time"]))
+
+print(tabulate(music,["Artist","Album", "Year", "Genre", "Time",""]))
 
 
