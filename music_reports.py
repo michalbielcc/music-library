1cbffb323da3c54c12b291d9e7568f2e420f4cf2
Michal Biel <mierwew@gmail.com>
2018-10-09 12:23:05 +0200

    export and album library

---
+++ music_reports.py
@@ -0,0 +1,17 @@
+music = []
+
+def import_music(music, filename='text_albums_data.txt'):
+    txt = open(filename, 'r')
+    music1 = []
+    for line in txt:
+        i = line.split(',')
+        music1.append(i)
+    for i in music1:
+        music.append(i)
+    txt.close()
+
+import_music(music, filename='text_albums_data.txt')
+
+
+
+print(music)
