46ce67c6e4eddb00e8a971fb0f71aff4a533cf09
KamilBracki <kamilbracki1@gmail.com>
2018-10-09 13:50:21 +0200

    tabelkadopoprawy

--- music_reports.py
+++ music_reports.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate
 music = []
 
 def import_music(music, filename='text_albums_data.txt'):
@@ -6,12 +7,16 @@ def import_music(music, filename='text_albums_data.txt'):
     for line in txt:
         i = line.split(',')
         music1.append(i)
-    for i in music1:
+    for i in music1
         music.append(i)
     txt.close()
 
 import_music(music, filename='text_albums_data.txt')
 
 
+from tabulate import tabulate
 
-print(music)
+
+print(tabulate(music,["Album", "Artist", "Year", "Genre", "Time"]))
+
+
