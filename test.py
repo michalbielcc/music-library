3d556a7641b38fd433a49a6b154ce91b53faf163
KamilBracki <kamilbracki1@gmail.com>
2018-10-10 11:43:18 +0200

    aaa

--- test.py
+++ test.py
@@ -1,34 +1,61 @@
-music = [['Pink Floyd', 'The Dark Side Of The Moon', '1973', 'progressive rock', '43:00', '\n'], ['Britney Spears', 'Baby One More Time', '1999', 'pop', '42:20', '\n'], ['The Beatles', 'Revolver', '1966', 'rock', '34:43', '\n'], ['Deep Purple', 'Machine Head', '1972', 'hard rock', '37:25', '\n'], ['Old Timers', 'Old Time', '1966', 'ancient', '123:45', '\n'], ['Pink Floyd', 'Wish You Were Here', '1975', 'progressive rock', '44:28', '\n'], ['Boston', 'Boston', '1976', 'hard rock', '37:41', '\n'], ['Monika Brodka', 'Granada', '2010', 'pop', '37:42', '\n'], ['David Bowie', 'Low', '1977', 'rock', '38:26', '\n'], ['Massive Attack', 'Blue Lines', '1991', 'hip hop', '45:02', '\n']]
+music = [['Pink Floyd', 'The Dark Side Of The Moon', '1973', 'progressive rock', '43:00'], ['Britney Spears', 'Baby One More Time', '1999', 'pop', '42:20'], ['The Beatles', 'Revolver', '1966', 'rock', '34:43'], ['Deep Purple', 'Machine Head', '1972', 'hard rock', '37:25'], ['Old Timers', 'Old Time', '1966', 'ancient', '123:45'], ['Pink Floyd', 'Wish You Were Here', '1975', 'progressive rock', '44:28'], ['Boston', 'Boston', '1976', 'hard rock', '37:41'], ['Monika Brodka', 'Granada', '2010', 'pop', '37:42'], ['David Bowie', 'Low', '1977', 'rock', '38:26'], ['Massive Attack', 'Blue Lines', '1991', 'hip hop', '45:02']]
 genre_list = []
 time_list = []
+length_list = []
+longest_shortest = []
 
 def find_genre(genre):
-    genre_list1 = []
     for x in music:
         if genre in x:
-            genre_list1.append(x)
+            genre_list.append(x)
 
-    for i in genre_list1:
-        genre_list.append(i)
+
 
 #find_genre('pop')
-
-
-
-
+#print(genre_list)
 
 def find_time_range(year, year2):
-    time_list1 = []
     for x in music:
         if int(x[2]) in range(int(year), int(year2)+1):
-            time_list1.append(x)
+            time_list.append(x)
 
-    for i in time_list1:
-        time_list.append(i)    
+  
 
-find_time_range('1972','1973')
-
-print(time_list)
+#find_time_range('1972','1973')
+#print(time_list)
 
 def find_longest_shortest():
+    length_list1 = []
+    length_list2 = []
+    for x in music:
+        length_list1.append(x[4])
+    for x in length_list1:
+        y = x.split(':')
+        x = ''.join(y)
+        length_list2.append(int(x))
+
+    length_list1 =  sorted(length_list2)
+    print(length_list1)
+
+    min = list(str(length_list1[0]))
+    min.insert(-2, ':')
+    min = ''.join(min)
+    for x in music:
+        if x[4] == min:
+            longest_shortest.append(x)
+
+    max = list(str(length_list1[-1]))
+    max.insert(-2, ':')
+    max = ''.join(max)
+    for x in music:
+        if x[4] == max:
+            longest_shortest.append(x)
+
+#find_longest_shortest()
+#print(longest_shortest)
+
+
+
+
+
     
